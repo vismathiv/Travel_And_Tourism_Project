0fdc16f879ed61275bafbfaae52fbbe38ffd060d
vismathi v <vismathiv26@gmail.com>
2022-03-28 10:18:40 +0530

    updated 10:18

--- PackageAllDetails.cs
+++ PackageAllDetails.cs
@@ -61,7 +61,7 @@ namespace Travel_And_Tourism_Project
                     },
                     new Packages
                     {
-                        PName="Phi-Phi-Island",//
+                        PName="Kashmir Holiday with Airfare",//
                         PPrice=12000,
                         PDestination="Kashmir",
                         PNoofdays=4,
@@ -71,7 +71,7 @@ namespace Travel_And_Tourism_Project
                     },
                     new Packages
                     {
-                        PName="Phi-Phi-Island",//
+                        PName="Coonoor Tour",//
                         PPrice=15000,
                         PDestination="ooty",
                         PNoofdays=5,
@@ -81,20 +81,103 @@ namespace Travel_And_Tourism_Project
                     },
                     new Packages
                     {
-                        PName="Phi-Phi-Island",//
+                        PName="Local Sight Seeing Tour Package",//
                         PPrice=18000,
-                        PDestination="kulumanar",
+                        PDestination="kulumanali",
                         PNoofdays=4,
-                        PStartdate=DateTime.Parse("01-08-2022"),
-                        PEnddate=DateTime.Parse("01-09-2022"),
+                        PStartdate=DateTime.Parse("01-07-2022"),
+                        PEnddate=DateTime.Parse("01-08-2022"),
                         PcatId=2
                     },
+                    new Packages
+                    {
+                        PName="Boracay Package",
+                        PPrice=60000,
+                        PDestination="Philliphines",
+                        PNoofdays=4,
+                        PStartdate=DateTime.Parse("01-12-2022"),
+                        PEnddate=DateTime.Parse("01-01-2023"),
+                        PcatId=3 // for rainy category
+                    },
+                    new Packages
+                    {
+                        PName="Centra Grand Beach",
+                        PPrice=50000,
+                        PDestination="Thailand",
+                        PNoofdays=4,
+                        PStartdate=DateTime.Parse("01-12-2022"),
+                        PEnddate=DateTime.Parse("01-01-2023"),
+                        PcatId=3 // for rainy category
+                    },
+
                 };
             }
             catch(Exception ex)
             {
                 WriteLine("Error in insertion " + ex.Message);
             }
+        }
+
+        public void insertpackageddetails()
+        {
+            List<PackageDetails> lstdetails = new List<PackageDetails>()
+            {
+                new PackageDetails
+                {
+                    PId=101,
+                    PdDescription="This is a coconut tree vacation package which is allocated in maldive",
+                    PdDays=6
+                },
+                new PackageDetails
+                {
+                    PId=102,
+                    PdDescription="This is a kani palm beach vacation package which is allocated in maldive",
+                    PdDays=6
+                },
+                new PackageDetails
+                {
+                    PId=103,
+                    PdDescription="This is a phi-phi island located in bangkok",
+                    PdDays=10
+                },
+                new PackageDetails
+                {
+                    PId=104,
+                    PdDescription="This is pattaya coral island located in bangkok",
+                    PdDays=10
+                },
+                new PackageDetails
+                {
+                    PId=105,
+                    PdDescription="This is kashmir holiday with airfare in kashmir",
+                    PdDays=4
+                },
+                new PackageDetails
+                {
+                    PId=106,
+                    PdDescription="This is coonor tour located in ooty",
+                    PdDays=5
+                },
+                new PackageDetails
+                {
+                    PId=107,
+                    PdDescription="This is Local Sight Seeing Tour Package in kulumanali",
+                    PdDays=4
+                },
+                new PackageDetails
+                {
+                    PId=108,
+                    PdDescription="This is Boracay Package in philliphines",
+                    PdDays=4
+                },
+                new PackageDetails
+                {
+                    PId=109,
+                    PdDescription="Centra Grand Beach in Thailand",
+                    PdDays=4
+                }
+
+            };
         }
     }
 }
